f66093d67063acf0acd4580125b7c158b07ef9ec
早起喝燕麦 <zhuowangyi@163.com>
2025-01-21 11:10:12 +0800

    国际化

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'normalize.css';
-import 'element-plus/dist/index.css'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 
+import "normalize.css";
+import "element-plus/dist/index.css";
+import "./style.css";
+import App from "./App.vue";
 
-const app = createApp(App)
-app.use(ElementPlus)
-app.mount('#app')
+const app = createApp(App);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+app.mount("#app");
