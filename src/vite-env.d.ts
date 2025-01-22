af70d86e39f75a1f21a889292f756850e6af09c3
早起喝燕麦 <zhuowangyi@163.com>
2025-01-22 13:19:32 +0800

    format

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,22 +1,20 @@
 /// <reference types="vite/client" />
 declare module "element-plus/dist/locale/zh-cn.mjs";
 declare module "element-plus/dist/locale/en.mjs";
-declare module '*.vue' {
-    import type { DefineComponent } from 'vue'
-    const component: DefineComponent<{}, {}, any>
-    export default component
-  }
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
 
-
-  declare module "vuex" {
-    export * from "vuex/types/index.d.ts";
-    export * from "vuex/types/helpers.d.ts";
-    export * from "vuex/types/logger.d.ts";
-    export * from "vuex/types/vue.d.ts";
-  
+declare module "vuex" {
+  export * from "vuex/types/index.d.ts";
+  export * from "vuex/types/helpers.d.ts";
+  export * from "vuex/types/logger.d.ts";
+  export * from "vuex/types/vue.d.ts";
 }
 // types/cookies.d.ts
-declare module 'js-cookie' {
+declare module "js-cookie" {
   const Cookies: any;
   export default Cookies;
 }
