26535d54272d88577f46353d44f901c634dce5f8
早起喝燕麦 <zhuowangyi@163.com>
2025-01-22 11:27:05 +0800

    rename

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import "element-plus/dist/index.css";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-import store from "./sotre";
+import store from "./store";
 const app = createApp(App);
 app.use(ElementPlus, {
   locale: zhCn,
