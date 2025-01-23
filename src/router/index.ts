c10cd873a5790b054a4a2373bb9eb1c0004d2511
早起喝燕麦 <zhuowangyi@163.com>
2025-01-23 11:09:15 +0800

    修改为hash模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,34 @@
-import { createRouter, createWebHistory } from "vue-router";  // 使用 createWebHistory
+import { createRouter, createWebHashHistory } from "vue-router"; // 使用 createWebHistory
 
 import HomeView from "@/views/home/index.vue";
 import AboutView from "@/views/about/index.vue";
 
-
-export const asyncRoutes = []
+export const asyncRoutes = [];
 export const constantRoutes = [
   {
     path: "/login",
     component: () => import("@/views/login/index.vue"),
     hidden: true,
   },
-]
-
-
+];
 
 const routes = [
   { path: "/", component: HomeView },
   { path: "/about", component: AboutView },
+  {
+    path: "/dashboard",
+    component: () => import("@/views/dashboard/index.vue"),
+    name: "Dashboard",
+    meta: { title: "Dashboard", icon: "dashboard", affix: true },
+  },
+
 ];
 
 const router = createRouter({
-  history: createWebHistory(),  // 使用 createWebHistory
+  history: createWebHashHistory(), // 使用 createWebHashHistory
   routes,
 });
 
-export function resetRouter() {
-  
-}
+export function resetRouter() {}
 
 export default router;
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+          {{message}}
+    </div>
+</template>
+
+<script setup>
+ import {ref} from 'vue';
+
+ let message = ref('我是看板222')
+
+</script>
